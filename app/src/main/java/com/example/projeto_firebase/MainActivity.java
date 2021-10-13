2ca71e304d1d801dd3364e623ae9bfc32b125640
Alberto <antonioalbertodsb@gmail.com>
2021-10-12 21:39:23 -0300

    select

--- app/src/main/java/com/example/projeto_firebase/MainActivity.java
+++ app/src/main/java/com/example/projeto_firebase/MainActivity.java
@@ -6,14 +6,20 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 
 import com.example.projeto_firebase.modelo.Aluno;
 import com.google.firebase.FirebaseApp;
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
 
+import java.util.ArrayList;
+import java.util.List;
 import java.util.UUID;
 
 public class MainActivity extends AppCompatActivity {
@@ -23,6 +29,9 @@ public class MainActivity extends AppCompatActivity {
 
     FirebaseDatabase firebaseDatabase;
     DatabaseReference databaseReference;
+
+    private List<Aluno> list_Aluno = new ArrayList<Aluno>();
+    private ArrayAdapter<Aluno> arrayAdapterAluno;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,11 +43,34 @@ public class MainActivity extends AppCompatActivity {
         listV_dados = (ListView) findViewById(R.id.listV_dados);
 
         inicializarFirebase();
+        eventoDatabase();
+    }
+
+    private void eventoDatabase(){
+        databaseReference.child("Aluno").addValueEventListener(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot snapshot) {
+                list_Aluno.clear();
+                for (DataSnapshot objSnapshot:snapshot.getChildren()){
+                    Aluno aluno = objSnapshot.getValue(Aluno.class);
+                    list_Aluno.add(aluno);
+                }
+                arrayAdapterAluno = new ArrayAdapter<Aluno>(MainActivity.this,
+                        android.R.layout.simple_list_item_1, list_Aluno);
+                listV_dados.setAdapter(arrayAdapterAluno);
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError error) {
+
+            }
+        });
     }
 
     private void inicializarFirebase(){
         FirebaseApp.initializeApp(MainActivity.this);
         firebaseDatabase = FirebaseDatabase.getInstance();
+        firebaseDatabase.setPersistenceEnabled(true);
         databaseReference = firebaseDatabase.getReference();
     }
 
